8325160902e8434d50319780d3e0b5eaaaa5ee0d
Alexandr M <samsungi777s@gmail.com>
2022-10-16 15:35:41 +0300

    Setup project, added eslint

--- README.md
+++
@@ -1,1 +0,0 @@
-# movies_apollo_graphql
---
+++ client/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import CssBaseline from '@mui/material/CssBaseline';
+
+function App() {
+  return (
+    <>
+      <CssBaseline />
+
+      <div>Movies</div>
+    </>
+  );
+}
+
+export default App;
