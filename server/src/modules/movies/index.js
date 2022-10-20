4b7e912be14d49be4b34045374e7857cc9b07daf
Alexandr M <samsungi777s@gmail.com>
2022-10-20 20:49:20 +0300

    Added pagination

--- server/src/modules/movies/index.js
+++ server/src/modules/movies/index.js
@@ -2,9 +2,9 @@ const axios = require('axios');
 const {Movies} = require('./entities/Movies')
 const {API_KEY} = require('../../config/index')
 
-const url = `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}&language=en-US&page=1`;
 
-const getPopular = async () => {
+const getPopular = async (page) => {
+    const url = `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}&language=en-US&page=${page}`;
    const result = await axios.get(url);
    return new Movies(result.data);
 }
