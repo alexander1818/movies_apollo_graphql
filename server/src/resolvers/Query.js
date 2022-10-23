ce65c9c97abefbdf8a3bd3713ca069d45d8b5937
Alexandr M <samsungi777s@gmail.com>
2022-10-23 21:06:29 +0300

    Added query moviesByIds

--- server/src/resolvers/Query.js
+++ server/src/resolvers/Query.js
@@ -1,10 +1,18 @@
-const {getPopular} = require('../modules/movies')
+const {getPopular, getMoviesByIds} = require('../modules/movies')
+const {Movie} = require("../modules/movies/entities/Movie");
 
 async function movies(parent, args) {
     const data = await getPopular(args.page);
     return data;
 }
 
+async function moviesByIds(parent, { ids }) {
+    const request = ids.map((id) => getMoviesByIds(id));
+    const data = await Promise.all(request)
+    const movies = data.map(({data}) => new Movie(data))
+    return movies;
+}
+
 module.exports = {
-    movies
+    movies, moviesByIds
 }
