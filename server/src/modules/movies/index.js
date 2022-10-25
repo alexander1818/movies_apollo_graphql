4c3cbdeec4a19937f2929d257278ad229b2328ca
Alexandr M <samsungi777s@gmail.com>
2022-10-25 14:22:50 +0300

    Added internationalization request

--- server/src/modules/movies/index.js
+++ server/src/modules/movies/index.js
@@ -4,16 +4,14 @@ const {API_KEY} = require('../../config/index')
 
 const BASE_API_URL = 'https://api.themoviedb.org/3'
 
-
-const getPopular = async (page) => {
-    const url = `${BASE_API_URL}/movie/popular?api_key=${API_KEY}&language=en-US&page=${page}`;
+const getPopular = async (page, language) => {
+    const url = `${BASE_API_URL}/movie/popular?api_key=${API_KEY}&language=${language}&page=${page}`;
    const result = await axios.get(url);
    return new Movies(result.data);
 }
-const getMoviesByIds = (id) => {
-    const url = `${BASE_API_URL}/movie/${id}?api_key=${API_KEY}&language=en-US`;
+const getMoviesByIds = (id, language) => {
+    const url = `${BASE_API_URL}/movie/${id}?api_key=${API_KEY}&language=${language}`;
     return  axios.get(url);
-   // return new Movies(result.data);
 }
 
 module.exports = {
--- server/src/resolvers/Query.js
+++ server/src/resolvers/Query.js
@@ -1,13 +1,13 @@
 const {getPopular, getMoviesByIds} = require('../modules/movies')
 const {Movie} = require("../modules/movies/entities/Movie");
 
-async function movies(parent, args) {
-    const data = await getPopular(args.page);
+async function movies(parent, args, { locale }) {
+    const data = await getPopular(args.page, locale);
     return data;
 }
 
-async function moviesByIds(parent, { ids }) {
-    const request = ids.map((id) => getMoviesByIds(id));
+async function moviesByIds(parent, { ids }, { locale }) {
+    const request = ids.map((id) => getMoviesByIds(id, locale));
     const data = await Promise.all(request)
     const movies = data.map(({data}) => new Movie(data))
     return movies;
